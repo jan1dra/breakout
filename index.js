315eab8b0d513088f75ec392bfcbf180db5a7b25
jan1dra <100887546+jan1dra@users.noreply.github.com>
2022-05-31 16:36:48 -0700

    functionality

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const grid = document.querySelector(".grid")
+const scoreDisplay = document.querySelector("#score")
 const blockWidth = 100
 const blockHeight = 20
+const boardWidth = 560
+const boardHeight = 300
+const ballDiameter = 20
+let timerId 
+let xDirection = -2
+let yDirection = 2
+let score = 0
+
 
 const userStart = [230,10]
 let currentPosition = userStart
+
+const ballStart = [270,40]
+let ballCurrentPosition = ballStart
 
 class Block {
     constructor(xAxis, yAxis) {
@@ -57,18 +69,122 @@ function drawUser() {
     user.style.bottom = currentPosition [1] + "px"
 }
 
+//draw the ball
+function drawBall() {
+    ball.style.left = ballCurrentPosition [0] + "px"
+    ball.style.bottom = ballCurrentPosition[1] + "px"
+
+}
 //move user
 function moveUser(e) {
     switch(e.key) {
         case "ArrowLeft":
+            if(currentPosition[0] > 0){
             currentPosition[0] -= 10
             drawUser()
+            }
+            break;
+        case "ArrowRight":
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition [0] +=10
+                drawUser()
+            }
             break;
     }
 }
 
 document.addEventListener("keydown", moveUser)
 
-exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+//add ball
+const ball = document.createElement("div")
+ball.classList.add("ball")
+ball.style.left = ballCurrentPosition[0] + "px"
+ball.style.bottom = ballCurrentPosition[1] + "px"
+drawBall()
+grid.appendChild(ball)
 
-exports.build = series(scssTask, jsTask);
+//move ball
+function moveBall(){
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+    checkForCollisions()
+}
+
+timerId = setInterval(moveBall, 30)
+
+//check for collisions
+function checkForCollisions() {
+    //check for block collisons
+    for (let i = 0; i < blocks.length; i++) {
+      if (
+          (ballCurrentPosition[0] > blocks[i].bottomLeft[0] &&
+          ballCurrentPosition[0] < blocks[i].bottomRight[0]) && 
+          (ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && 
+          ballCurrentPosition[1] < blocks[i].topLeft[1]
+         ){
+          const allBlocks = Array.from(document.querySelectorAll(".block"))
+          allBlocks[i].classList.remove("block")
+          blocks.splice(i, 1)
+          changeDirection()
+          score++
+          scoreDisplay.innerHTML = score
+        //check for win
+        if(blocks.length === 0) {
+            scoreDisplay.innerHTML = "YOU'RE A WINNER!"
+            clearInterval(timerId)
+            document.removeEventListener("keydown", moveUser)
+        }
+      }
+    }
+
+
+    // check for wall collisions
+    if (
+    ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
+    ballCurrentPosition[1] >= (boardHeight - ballDiameter) ||
+    ballCurrentPosition[0] <= 0
+    ) {
+      changeDirection()  
+    }
+    
+    //check for user collisions
+    if (
+        (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) &&
+        (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight)
+    ){
+        changeDirection()
+    }
+    
+    //check for game over
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = "YOU'RE A LOSER!" 
+        document.removeEventListener("keydown", moveUser)
+    }
+}
+function changeDirection() {
+if (xDirection === 2 && yDirection ===2){
+    yDirection = -2
+    return
+}
+if (xDirection === 2 && yDirection === -2) {
+  xDirection = -2
+  return  
+}
+if (xDirection === -2 && yDirection === -2){
+    yDirection = 2
+    return
+}
+if (xDirection === -2 && yDirection === 2){
+xDirection = 2
+return
+}
+}
+
+
+
+
+//exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+//exports.build = series(scssTask, jsTask);
